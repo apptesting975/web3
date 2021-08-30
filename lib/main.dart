f17e7e601aa4d825ab1c11748567a63178a07c90
apptesting975 <apptesting975@gmail.com>
2021-08-30 14:45:45 +0545

    changed

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'web3'),
+      home: MyHomePage(title: 'web3 yo'),
     );
   }
 }
---
+++ lib/privacy.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Privacy extends StatefulWidget {
+  const Privacy({Key? key}) : super(key: key);
+
+  @override
+  _PrivacyState createState() => _PrivacyState();
+}
+
+class _PrivacyState extends State<Privacy> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
