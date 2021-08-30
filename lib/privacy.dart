9eacb12657d39734801646dd8050d483f92b5d6f
apptesting975 <apptesting975@gmail.com>
2021-08-30 15:58:29 +0545

    new

--- lib/privacy.dart
+++ lib/privacy.dart
@@ -10,6 +10,44 @@ class Privacy extends StatefulWidget {
 class _PrivacyState extends State<Privacy> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+        body: ListView(
+          children: [
+            Divider(
+              height: 10,
+            ),
+            Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Text(
+                "Free Fire",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20,
+                ),
+              ),
+            ),
+            Divider(
+              height: 20,
+              color: Colors.black,
+            ),
+            Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Text(
+                "You can use our services in a variety of ways to manage your privacy. For example, you can sign up for a Google Account if you want to create and manage content like emails and photos, or see more relevant search results. And you can use many Google services when you’re signed out or without creating an account at all, like searching on Google or watching YouTube videos. You can also choose to browse the web privately using Chrome in Incognito mode. And across our services, you can adjust your privacy settings to control what we collect and how your information is used."
+
+                "To help explain things as clearly as possible, we’ve added examples, explanatory videos, and definitions for key terms. And if you have any questions about this Privacy Policy, you can contact us.",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20,
+                ),
+              ),
+            ),
+          ],
+        )
+    );
   }
 }
